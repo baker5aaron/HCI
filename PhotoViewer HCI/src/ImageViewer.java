427823620ccd07a512b1668d3c11831a5aa354fe
baker <baker5aaron@gmail.com>
2019-03-11 23:08:43 -0500

    idk

--- PhotoViewer HCI/src/ImageViewer.java
+++ PhotoViewer HCI/src/ImageViewer.java
@@ -13,7 +13,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
-import javax.swing.SpringLayout;
+import javax.swing.SwingConstants;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
@@ -26,9 +26,12 @@ public class ImageViewer {
 		final JFrame frame = new JFrame("Photo Viewer");
 		frame.getContentPane().setLayout(new MigLayout("", "[][][][][][][grow][][][][][][][]", "[][][][][grow][][][][]"));
 		
-		JScrollPane scrollPane = new JScrollPane();
-		frame.getContentPane().add(scrollPane, "cell 0 0 14 8,grow");
+		JLabel imgLabel = new JLabel();
 		
+		JScrollPane scrollPane = new JScrollPane(imgLabel);
+		imgLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		frame.getContentPane().add(scrollPane, "cell 0 0 14 8,grow");
+				
 		JButton btnPrevious = new JButton("Previous");
 		frame.getContentPane().add(btnPrevious, "cell 0 8");
 		
@@ -44,7 +47,7 @@ public class ImageViewer {
 		JMenu mnFile = new JMenu("   File   ");
 		menuBar.add(mnFile);
 		
-		JMenuItem mntmNewParentFile = new JMenuItem("New Parent File");
+		JMenuItem mntmNewParentFile = new JMenuItem("New Parent Folder");
 		mnFile.add(mntmNewParentFile);
 		
 		JMenuItem mntmNewImageFile = new JMenuItem("New Image File");
@@ -57,6 +60,28 @@ public class ImageViewer {
 		JMenuItem mntmNextImage = new JMenuItem("Next Image");
 		mntmNextImage.setMaximumSize(new Dimension(100,50));
 		menuBar.add(mntmNextImage);
+		
+		btnChooseImage.addActionListener(new ActionListener() {
+			public void actionPerformed(final ActionEvent e) {
+				
+				final JFileChooser chooser = new JFileChooser();
+				final int result = chooser.showOpenDialog(frame);
+				
+				if (result == JFileChooser.APPROVE_OPTION) {
+					final File f = chooser.getSelectedFile();
+					JOptionPane.showMessageDialog(frame, f.getAbsolutePath(), "Selection",
+							JOptionPane.INFORMATION_MESSAGE);
+					
+					try {
+						BufferedImage img = ImageIO.read(new File(f.getAbsolutePath()));
+						imgLabel.setIcon(new ImageIcon(img));
+						
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+				}
+			}
+		});
 		
 		frame.setVisible(true);
 
